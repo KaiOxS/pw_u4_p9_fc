6e440dbdcc48a7f4ade2549146a3c27772e4fe62
KaiOxS <torito9926@hotmail.es>
2026-01-28 19:47:59 -0500

    Comiit 2

---
+++ src/clients/MatriculaClient.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const consultarTodos = async () => {
+	const data = await axios.get('http://localhost:8081/matricula/api/v1.0/estudiantes').then(r => r.data);
+	return data;
+}
+
+export async function consumirTodosFacade() {
+	return await consultarTodos();
+}
