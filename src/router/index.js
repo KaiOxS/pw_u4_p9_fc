ef2cf0b308053d90f47ac29788497b7f16df466a
KaiOxS <torito9926@hotmail.es>
2026-02-05 18:43:19 -0500

    Taller36: Guardianes

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import {createRouter, createWebHashHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ActualizarParcialView from '@/views/ActualizarParcialView.vue'
 import ActualizarView from '@/views/ActualizarView.vue'
@@ -8,54 +8,89 @@ import ConsultarPorIdView from '@/views/ConsultarPorIdView.vue'
 import ConsultarTodosView from '@/views/ConsultarTodosView.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/actulizarParcial',
-    name: 'actulizarParcial',
-    component: ActualizarParcialView
-  },
-  {
-    path: '/actualizar',
-    name: 'actualizar',
-    component: ActualizarView
-  },
-  {
-    path: '/borrar',
-    name: 'borrar',
-    component: BorrarView
-  },
-  {
-    path: '/guardar',
-    name: 'guardar',
-    component: GuardarView
-  },
-  {
-    path: '/consultarId',
-    name: 'consultarId',
-    component: ConsultarPorIdView
-  },
-  {
-    path: '/consultarTodos',
-    name: 'consultarTodos',
-    component: ConsultarTodosView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+	{
+		path: '/',
+		name: 'home',
+		component: HomeView,
+		meta: {
+			requiereAutorizacion: true,
+			esPublica: false
+		}
+	},
+	{
+		path: '/actulizarParcial',
+		name: 'actulizarParcial',
+		component: ActualizarParcialView,
+		meta: {
+			requiereAutorizacion: true,
+			esPublica: false
+		}
+
+	},
+	{
+		path: '/actualizar',
+		name: 'actualizar',
+		component: ActualizarView,
+		meta: {
+			requiereAutorizacion: true,
+			esPublica: false
+		}
+	},
+	{
+		path: '/borrar',
+		name: 'borrar',
+		component: BorrarView,
+		meta: {
+			requiereAutorizacion: true,
+			esPublica: false
+		}
+	}, {
+		path: '/guardar',
+		name: 'guardar',
+		component: GuardarView,
+		meta: {
+			requiereAutorizacion: true,
+			esPublica: false
+		}
+	}, {
+		path: '/consultarId',
+		name: 'consultarId',
+		component: ConsultarPorIdView,
+		meta: {
+			requiereAutorizacion: false,
+			esPublica: false
+		}
+	}, {
+		path: '/consultarTodos',
+		name: 'consultarTodos',
+		component: ConsultarTodosView,
+		meta: {
+			requiereAutorizacion: false,
+			esPublica: false
+		}
+	}, {
+		path: '/about',
+		name: 'about',
+		// route level code-splitting
+		// this generates a separate chunk (about.[hash].js) for this route
+		// which is lazy-loaded when the route is visited.
+		component: () => import ( /* webpackChunkName: "about" */
+				'../views/AboutView.vue')
+	}
 ]
 
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes
-})
 
+const router = createRouter({history: createWebHashHistory(), routes})
+
+/*Configuracion del guardian de rutas*/
+router.beforeEach((to,from,next)=>{
+  if(to.meta.requiereAutorizacion){
+    console.log("Redirigiendo al Login")
+    /*Le envio a una página de login*/
+  } else{
+    console.log("Pase Libre")
+    next();
+    /*Le dejo que pase sin autenticación*/
+  }
+})
 export default router
